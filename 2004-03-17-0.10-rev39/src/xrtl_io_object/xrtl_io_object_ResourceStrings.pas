71eda5f686f8cdf0c728b0dfbe4645cc4729ca46
Illya Kysil <Illya Kysil@8151b8e4-99fc-cf4b-a0c8-eaa015f54a2e>
2004-04-02 10:33:51 +0000

    Copied remotely

---
+++ 2004-03-17-0.10-rev39/src/xrtl_io_object/xrtl_io_object_ResourceStrings.pas
@@ -0,0 +1,15 @@
+unit xrtl_io_object_ResourceStrings;
+
+{$INCLUDE xrtl.inc}
+
+interface
+
+resourcestring
+  SXRTLStreamerForClassNameRegistered   = 'Streamer for class name ''%s'' is already registered';
+  SXRTLStreamerForClassIdRegistered     = 'Streamer for class id ''%s'' is already registered';
+  SXRTLNoStreamerForClassNameRegistered = 'No streamer for class name ''%s'' registered';
+  SXRTLNoStreamerForClassIdRegistered   = 'No streamer for class id ''%s'' registered';
+
+implementation
+
+end.
